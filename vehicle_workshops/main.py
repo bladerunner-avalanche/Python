0c9caf38d67170ad8a7b1fa9744fc8ff0f774018
w4rl0ck-404 <117034924+w4rl0ck-404@users.noreply.github.com>
2022-11-16 16:33:46 +0100

    Update main.py

--- vehicle_workshops/main.py
+++ vehicle_workshops/main.py
@@ -1,22 +1,30 @@
 from workshop_data import data
 import functions as f
+print("--------------------")
+print("")
+print("Mileage statistics:")
+print("")
+print("Average mileage of all vehicles:", f.get_avg_miles(data))
+print("Average mileage of A-Class vehicles:", f.get_avg_miles(data, "A-Class"))
+print("Average mileage of G-Class vehicles:", f.get_avg_miles(data, "G-Class"))
+print("Average mileage of S-Class vehicles:", f.get_avg_miles(data, "S-Class"))
+print("--------------------")
+print("")
+print("Cost statistics:")
+print("")
+print("Average cost of all repairs:", f.get_avg_cost(data))
+print("Average cost of battery repairs:", f.get_avg_cost(data, "battery"))
+print("Average cost of engine repairs:", f.get_avg_cost(data, "engine"))
+print("Average cost of wheel repairs:", f.get_avg_cost(data, "wheel"))
+print("--------------------")
+print("")
+print("Vehicle type statistics:")
+print("")
+print("Number of each vehicle type:", f.get_vehicle_type_count(data))
+print("Number of each damage type:", f.get_damage_type_count(data))
+print("--------------------")
 
-avg_miles = f.get_avg_miles(data, "A-Class")
 
-print("The A-Class vehicles coming to our workshop are driven", avg_miles, "miles on average.")
-
-avg_miles = f.get_avg_miles(data)
-
-print("The vehicles coming to our workshop are driven", avg_miles, "miles on average.")
-
-avg_cost = f.get_avg_cost(data)
-print("The repairs cost", avg_cost, "USD on average.")
-avg_cost = f.get_avg_cost(data, "battery")
-print("avg cost for battery repairs is", avg_cost, "USD")
-
-print(f.get_vehicle_type_count(data))
-damage_count = f.get_damage_type_count(data)
-print(max(damage_count, key=damage_count.get), "is the most common damage type.")
 
 
 
